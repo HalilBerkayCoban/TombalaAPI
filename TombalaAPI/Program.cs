64b729cbc9c591499f36b2896e563ece55d1cbb4
Berkay Coban <hberkaycoban@gmail.com>
2023-06-05 23:11:48 +0300

    MongoDB database settings created.

--- TombalaAPI/Program.cs
+++ TombalaAPI/Program.cs
@@ -1,3 +1,5 @@
+using TombalaAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<DatabaseSettings>(
+    builder.Configuration.GetSection("TombalaDatabase"));
 
 var app = builder.Build();
 
