84323d7f45e69d868c03a2fa8763a6a313a6b1cc
Berkay Coban <hberkaycoban@gmail.com>
2023-06-07 23:19:30 +0300

    Discord Authentication implemented

--- TombalaAPI/Program.cs
+++ TombalaAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
 using TombalaAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +16,33 @@ builder.Services.AddSwaggerGen();
 builder.Services.Configure<DatabaseSettings>(
     builder.Configuration.GetSection("TombalaDatabase"));
 
+builder.Services.AddAuthentication("cookie").AddCookie("cookie").AddOAuth("Discord", options =>
+{
+    options.SignInScheme = "cookie";
+    options.ClientId = builder.Configuration.GetValue<string>("Discord:ClientId");
+    options.ClientSecret = builder.Configuration.GetValue<string>("Discord:ClientSecret");
+    options.AuthorizationEndpoint = "https://discord.com/oauth2/authorize";
+    options.TokenEndpoint = "https://discord.com/api/oauth2/token";
+    options.CallbackPath = "/oauth/discord-cb";
+    options.Scope.Add("identify");
+    options.SaveTokens = true;
+    options.UserInformationEndpoint = "https://discord.com/api/users/@me";
+    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "username");
+
+    options.Events.OnCreatingTicket = async ctx =>
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint);
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
+
+        var response = await ctx.Backchannel.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        var user = await response.Content.ReadFromJsonAsync<JsonElement>();
+        ctx.RunClaimActions(user);
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,9 +52,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapGet("/login", () =>
+{
+    return Results.Challenge(new AuthenticationProperties
+    {
+        RedirectUri = "your-redirect-uri"
+    }, authenticationSchemes: new List<string>() { "Discord" });
+});
 
 app.MapControllers();
 
